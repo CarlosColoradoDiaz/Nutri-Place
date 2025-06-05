8ba2b7a4468674b8b27a4421e420cae7de19a0f7
Carlos Colorado <carlosandrescolorado09@gmail.com>
2025-06-05 18:39:54 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,20 @@
+//Evento al dar CLICK en ordenar esta
+document.querySelectorAll('.cta-boton').forEach(boton => {
+    boton.addEventListener('click', () => {
+        alert('¡Gracias por tu interés! Serás redirigido a WhatsApp para seguir con tu Proceso.');
+    });
+});
+
+//Cambiar texto motivacional
+const fraseTexto = document.getElementById('frasetexto');
+const frases = [
+    "La salud es la mayor riqueza.",
+    "Cada pequeño paso cuenta.",
+    "Cuida tu cuerpo, es tu templo.",
+    "Nutrirte bien es amarte más.",
+];
+let index = 0;
+setInterval(() => {
+    fraseTexto.textContent = frases[index];
+    index = (index + 1) % frases.length;
+}, 5000);
